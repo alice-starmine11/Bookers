cb469e57a5ef1a52acbc5bf078a3158d3c2cecb4
alice-starmine11 <121926699+alice-starmine11@users.noreply.github.com>
2023-01-15 07:55:50 +0000

    [Add] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,12 @@
 class BooksController < ApplicationController
-  def new
+
+  def index
+    @books = Book.all
     @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -9,16 +15,8 @@ class BooksController < ApplicationController
        flash[:notice] = "Book was successfully created."
        redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
-  end
-
-  def index
-    @books = Book.all
-  end
-
-  def show
-    @book = Book.find(params[:id])
   end
 
   def edit
@@ -26,16 +24,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
 
 private
